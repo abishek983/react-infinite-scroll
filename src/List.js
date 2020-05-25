5ed8759e7d0a5cc26ef36d7f077da2c363e594d2
abishek <abishekwwe@gmail.com>
2020-05-25 22:22:12 +0530

    made the code reusable

--- src/List.js
+++ src/List.js
@@ -1,23 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import useInfiniteScroll from './useInfiniteScroll'
 
 const List = () => {
     const [listItems, setListItems] = useState(Array.from(Array(30).keys(), n => n + 1));
-    const [isFetching, setIsFetching] = useState(false);
-
-    useEffect(() =>{
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll',handleScroll);
-    }, [])
-
-    function handleScroll(){
-        if(window.innerHeight + document.documentElement.scrollTop!==document.documentElement.offsetHeight) return;
-        setIsFetching(true);
-    }
-
-    useEffect(() => {
-        if(!isFetching) return;
-        fetchMoreListItems();
-    },[isFetching])
+    const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
 
     function fetchMoreListItems(){
         setTimeout(() =>{
@@ -30,6 +16,7 @@ const List = () => {
       <>
         <ul className="list-group mb-2">
           {listItems.map(listItem => <li  key={listItem} className="list-group-item">List Item {listItem}</li>)}
+          {isFetching && <p>Fetching More items...</p>}
         </ul>
       </>
     );
